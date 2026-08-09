7b9b666680f57f4d7d4c3f64b0f20828f4894e9d
Mohsen Waheed Elsisi <mohsenelsisi36@gmail.com>
2026-08-09 03:24:27 +0300

    added Moved files from @mohsen-w-elsisi/resume-generator to this repo

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export type { Parser } from "./parser.js";
+export { ProjectsParser, Project } from "./projects.js";
+export { ToolsParser, Tool } from "./tools.js";
+export { PlatformsParser, Platform } from "./platforms.js";
